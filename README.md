774ba8f1180c3837388fc407441c38ac721e3eed
Justin Santos <jgsant20@gmail.com>
2021-03-22 19:32:46 -0500

    Added base for flask-react app

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# databases-music-library
-Music library made for COSC 3380 databases.
+## Dependencies
+- python 3.8
+- node 14.16.0
+- npm 6.14.8
+- flaskpy
+
+Not sure if these are hard requirements, but this is what I was running. ~Justin
+
+## How-to run app
+1. Run the following commands
+  - `git clone https://github.com/jgsant20/databases-music-library.git`
+  - `cd databases-music-library`
+  - `yarn`
+  - `pip install flask`
+5. Open up terminal, type `yarn start`
+6. Open up second terminal, type `yarn start-api`
+7. Pro$$it
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('./time').then(res => res.json()).then(date => {
+      setCurrentTime(date.time);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          The current time is {currentTime}.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
