320ebe4019781051300aa58409d806a76e7ba2a5
Dylan-Buras <65236201+Dylan-Buras@users.noreply.github.com>
2021-04-05 12:02:07 -0500

    Dylan Test 1

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
 
 	return (
 		<div>
+			<h1>Hello, world!</h1>
+			<h2>It is {new Date().toLocaleDateString()}.</h2>
 			<p>Testing, this is the current time! {isPending}</p>
 		</div>
 	)
