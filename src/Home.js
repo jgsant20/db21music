26468cabbd68dc5b05b2088e323b1e2a14853644
Justin Santos <jgsant20@gmail.com>
2021-04-05 12:46:23 -0500

    test

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import useFetch from './useFetch';
 
+import PlayerBar from './components/player-bar/PlayerBar'
+
 const Home = () => {
   const { data, isPending, error } = useFetch('/api/time');
 
   console.log(data)
 
 	return (
-		<div>
-			<p>Testing, this is the current time! {isPending}</p>
-		</div>
+		<>
+			<div>
+				<p>Testing, this is the current time! {isPending}</p>
+			</div>
+			<PlayerBar />
+		</>
 	)
 }
 
