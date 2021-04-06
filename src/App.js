5f7fb1e7394310ab11d57bbe99de3684435da4ab
Dylan-Buras <65236201+Dylan-Buras@users.noreply.github.com>
2021-04-06 13:20:41 -0500

    Forms update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Home from './Home';
 import Login from "./components/Login/Login"
+import SongSubmission from "./components/Song-Submission/Song-submission";
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 const App = () => (
   <>
-    <BrowserRouter basename = "/login">
+    <BrowserRouter>
       <Link to="/home">Home</Link>
       <Link to="/login">Login</Link>
+      <Link to = "/songsubmission">SongSubmission</Link>
       <Switch>
         <Route exact path="/home">
           <h1>{process.env.API_URL}</h1>
@@ -16,6 +18,10 @@ const App = () => (
        <Route exact path="/login">
           <h1>{process.env.API_URL}</h1>
           <Login />
+        </Route>
+        <Route exact path="/songsubmission">
+          <h1>{process.env.API_URL}</h1>
+          <SongSubmission />
         </Route>
       </Switch>
     </BrowserRouter>
