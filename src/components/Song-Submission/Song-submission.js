cc9673b71506052e13bb23542d6236a540425469
Dylan-Buras <65236201+Dylan-Buras@users.noreply.github.com>
2021-04-06 20:28:53 -0500

    Added File Submission

--- src/components/Song-Submission/Song-submission.js
+++ src/components/Song-Submission/Song-submission.js
@@ -9,8 +9,6 @@ const SongSubmission = () => {
         [e.target.songName]: [e.target.value]
 })
 
-    const [selectedFile, setSelectedFile] = useState(null);
-
     const blankContributor = { name: "" };
     const [contributorState, setContributorState] = useState([
         { ...blankContributor },
@@ -26,8 +24,37 @@ const SongSubmission = () => {
         setContributorState(updatedContributors);
     };
 
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+    const fileChangeHandler = (e) => {
+        setSelectedFile(e.target.files[0]);
+        setIsFilePicked(true);
+    };
+
+    const handleSubmission = () => {
+        const formData = new FormData();
+
+        formData.append('File', selectedFile);
+
+        fetch(
+            //insert upload API
+            {
+                method: 'POST',
+                body: formData,
+            }
+        )
+            .then((response) => response.json())
+            .then((result) => {
+                console.log('Success: ', result);
+            })
+            .catch((error) => {
+                console.error('Error: ', error);
+            });
+
+    };
     return (
-        <form>
+        <form> 
             <label htmlFor="songName">Song Name</label>
             <input
                 type='text'
@@ -36,11 +63,29 @@ const SongSubmission = () => {
                 value={songNameState.songName}
                 onChange={handleSongNameChange}
                 />
+            <div>
+            <label htmlFor="file">Song File (MP3)</label>
+            </div>
+            <input type="file" name="file" onChange={fileChangeHandler} />
+            {isFilePicked ? (
+                <div>
+                    <p>Filename: {selectedFile.name}</p>
+                    <p>Filetype: {selectedFile.type}</p>
+                    <p>Size in bytes: {selectedFile.size}</p>
+                    <p>
+                        lastModifiedDate:{' '}
+                        {selectedFile.lastModifiedDate.toLocaleDateString()}
+                    </p>
+                </div>
+            ) : (
+                <p>Select a file to show details</p>
+            )}
             <input
                 type="button"
                 value="Add New Contributor"
                 onClick={addContributor}
-            />
+            /> 
+
             {
                 contributorState.map((val, idx)=> {
                     let contribID = `name-${idx}`
