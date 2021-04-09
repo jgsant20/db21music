01ab2229519a110e8a8a93a0fb28584ff0ffb293
Ethan Knutson <ethanknutson@Ethans-MacBook-Pro.local>
2021-04-08 23:09:35 -0500

    Cards

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,14 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid'
 
 import './Content.scss';
+import Card from "../songCard/songCard"
+import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
 
-const Content = () => {
+const useStyles = makeStyles((theme) => ({
+	root: {
+		marginLeft: 10,
+		marginRight: 10,
+	  flexGrow: 1,
+	},
+	card: {
+	  padding: theme.spacing(2),
+	  textAlign: 'center',
+	  color: theme.palette.text.secondary,
+	},
+  }));
+
+  export default function Content() {
+	const classes = useStyles();
 
 	return (
-		<div className="content">
-    </div>
+		<div className={classes.root}>
+			<h1>Songs</h1>
+		<Grid container spacing={3}>
+		  <Grid item xs={2}>
+			<Card className={classes.card}></Card>
+		  </Grid>
+		  <Grid item xs={2}>
+			<Card className={classes.card}></Card>
+		  </Grid>
+		  <Grid item xs={2}>
+			<Card className={classes.card}></Card>
+		  </Grid>
+		  <Grid item xs={2}>
+			<Card className={classes.card}></Card>
+		  </Grid>
+		  <Grid item xs={2}>
+			<Card className={classes.card}></Card>
+		  </Grid>
+		  <Grid item xs={2}>
+			<Card className={classes.card}></Card>
+		  </Grid>
+		</Grid>
+	  </div>
 	)
 }
 
-export default Content;
---
+++ src/components/songCard/songCard.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import Container from '@material-ui/core/Container';
+import { BorderAllOutlined } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'd-flex',
+    backgroundColor: 'white',
+    overflow: 'hidden',
+    border: "1.5px solid black",
+  },
+  details: {
+    display: 'grid',
+  },
+  mediaContainer: {
+    padding: 10,
+  },
+  cover: {
+    objectFit: 'cover',
+  },
+  content: {
+    display: 'grid',
+    padding: "5px 10px"
+  },
+  songName: {
+    fontSize: 15,
+  },
+  contributors: {
+    fontSize: 10,
+  },
+  controls: {
+    display: 'flex',
+    paddingBottom: "5px",
+    paddingLeft: "5px",
+
+  },
+  playIcon: {
+    height: 30,
+    width: 30,
+  },
+}));
+
+export default function songCard() {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <Card className={classes.root}>
+      <div container className={classes.mediaContainer}>
+        <CardMedia
+        className={classes.cover}
+        component="img"
+        src="https://cms-assets.tutsplus.com/uploads/users/114/posts/34296/image/Final-image.jpg"
+        title="Live from space album cover"
+      />
+      </div>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography className={classes.songName} component="h5" variant="h5">
+            Song Name
+          </Typography>
+          <Typography className={classes.contributors} variant="subtitle1" color="textSecondary">
+            Contributors
+          </Typography>
+        </CardContent>
+        <div className={classes.controls}>
+          <IconButton aria-label="play/pause">
+            <PlayArrowIcon className={classes.playIcon} />
+          </IconButton>
+        </div>
+      </div>
+    </Card>
+  );
+}
